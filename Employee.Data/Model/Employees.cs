4d0450843b78794287293dd2f03358266e8b3cf8
preetika <preetika.soni@stealth-us.com>
2022-06-13 02:32:07 -0400

    code added for emplWebAPI.Add project files.

---
+++ Employee.Data/Model/Employees.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeAPI.Data
+{
+    public class Employee
+    {
+
+         public int Id { get; set; }
+         public string FirstName { get; set; }
+         public string LastName { get; set; }
+         public string Department { get; set; }
+         public DateTime HireDate { get; set; }
+
+    }
+
+}
