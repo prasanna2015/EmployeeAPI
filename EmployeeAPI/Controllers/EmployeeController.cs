3e3559fe73576f9b0adb89a39873133b512fb5d4
preetika <preetika.soni@stealth-us.com>
2022-06-13 02:32:07 -0400

    Add project files.

---
+++ EmployeeAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using EmployeeAPI.Data;
+
+namespace EmployeeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private IEmployeeRepository employeeRepo = new EmployeeRepository();
+       
+
+        [HttpGet(Name = "GetAllEmployees")]
+        public ActionResult<IEnumerable<Employee>> GetAllEmployees()
+        {
+            return employeeRepo.GetAllEmployees();
+        }
+
+        [HttpGet]       
+        [Route("/[controller]/[action]/{id}")]
+        public ActionResult<Employee> GetEmployee(int id)
+        {
+            return Ok(employeeRepo.GetEmployee(id));
+        }
+
+        [HttpPost(Name = "AddEmployee")]
+        public ActionResult<bool> AddEmployee(Employee employee)
+        {
+            return employeeRepo.AddEmployee(employee);
+        }
+    }
+}
